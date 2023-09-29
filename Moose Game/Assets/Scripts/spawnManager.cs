67d33c27471a2518346c822dd07ce675be40c65b
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-09-29 22:04:23 +0100

    3 area spawning
    Created extra spawn points in method and used quaternion Euler to transform rotation of the instantiated objects

--- Moose Game/Assets/Scripts/spawnManager.cs
+++ Moose Game/Assets/Scripts/spawnManager.cs
@@ -6,9 +6,10 @@ public class spawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnPosZ = 20;
+    private float spawnPosZMin = -10;
     private float spawnRangeX = 20;
     private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    private float spawnInterval = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +26,18 @@ public class spawnManager : MonoBehaviour
 
     void SpawnRandomAnimal()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+ 
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+            Vector3 spawnPosTop = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(animalPrefabs[animalIndex], spawnPosTop, animalPrefabs[animalIndex].transform.rotation);
+  
+            Vector3 spawnPosRight = new Vector3(spawnRangeX, 0, Random.Range(spawnPosZMin, spawnPosZ));
+            Instantiate(animalPrefabs[animalIndex], spawnPosRight, Quaternion.Euler(0, -90, 0));
+  
+            Vector3 spawnPosLeft = new Vector3(-spawnRangeX, 0, Random.Range(spawnPosZMin, spawnPosZ));
+            Instantiate(animalPrefabs[animalIndex], spawnPosLeft, Quaternion.Euler(0, 90, 0));
+
+        
     }
 }
